f295a91ffd0512673cbf6fa1642af8402eb394f9
gui326 <guilherme.326@hotmail.com>
2022-06-26 13:24:57 -0300

    realizado cadastro cliente e empresas

--- src/main/java/gg/agiomoney/controller/ClientController.java
+++ src/main/java/gg/agiomoney/controller/ClientController.java
@@ -2,12 +2,24 @@ package gg.agiomoney.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import gg.agiomoney.model.Client;
+import gg.agiomoney.repository.ClientRepository;
+import gg.agiomoney.service.ClientService;
 
 @Controller
 public class ClientController {
+	
+	@Autowired
+	private ClientRepository clientRepository;
+	
+	@Autowired
+	private ClientService clientService;
 	
 	private static final Logger logger = LoggerFactory.getLogger(ClientController.class);
 	
@@ -23,7 +35,19 @@ public class ClientController {
 	public ModelAndView registerClient() {
 		logger.trace("Entrou em index");
 		ModelAndView mv = new ModelAndView("/client/registerClient");
-		logger.trace("Encaminhando para a view index");
+		mv.addObject("client", new Client());
+		return mv;
+	}
+	
+	@PostMapping("/client/register")
+	public ModelAndView registeredClient(Client client) {
+		logger.trace("Entrou em index");
+		ModelAndView mv = new ModelAndView("mensagem");
+		
+		clientService.saveClient(client);
+		
+		mv.addObject("mensagem", "Cadastro Realizado com sucesso!, bem-vindo " + client.getName());
+		
 		return mv;
 	}
 	
--- src/main/java/gg/agiomoney/controller/CompanyController.java
+++ src/main/java/gg/agiomoney/controller/CompanyController.java
@@ -2,14 +2,26 @@ package gg.agiomoney.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import gg.agiomoney.model.Company;
+import gg.agiomoney.repository.CompanyRepository;
+import gg.agiomoney.service.CompanyService;
 
 @Controller
 public class CompanyController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(CompanyController.class);
+	
+	@Autowired
+	private CompanyRepository companyRepository;
+	
+	@Autowired
+	private CompanyService companyService;
 	
 	@GetMapping("/company/login")
 	public ModelAndView loginCompany() {
@@ -23,6 +35,18 @@ public class CompanyController {
 	public ModelAndView registerCompany() {
 		logger.trace("Entrou em index");
 		ModelAndView mv = new ModelAndView("/company/registerCompany");
+		mv.addObject("company", new Company());
+		return mv;
+	}
+	
+	@PostMapping("/company/register")
+	public ModelAndView registeredCompany(Company company) {
+		logger.trace("Entrou em index");
+		ModelAndView mv = new ModelAndView("mensagem");
+		mv.addObject("mensagem", "Cadastro realizado com sucesso!, bem-vindo " + company.getName());
+		
+		companyService.saveCompany(company);
+		
 		logger.trace("Encaminhando para a view index");
 		return mv;
 	}
